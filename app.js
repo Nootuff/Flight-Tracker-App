86ff298449957720e34f5805e4f2f4c7c4971f77
Adam <adamwalkerlondon@gmail.com>
2021-03-02 21:05:35 +0000

    Basic Mapbox integration.

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require("path");
 const ejs = require('ejs');
+var MapboxClient = require('mapbox');
+require('dotenv').config()
+
+
 
 const app = express();
 app.set("views", path.join(__dirname, "/views"))
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
     res.render("home.ejs");
     });
+
 
 app.listen(3000, function () {
     console.log("Put http://localhost:3000 into the url bar to see your new app"); //String template literal, accuratley shows the port you are serving on Heroku or local. 
