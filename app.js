827f19f0281ded833b9e35fd6aff6ec54a34db31
Adam <adamwalkerlondon@gmail.com>
2021-03-02 08:48:06 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require("path");
+const ejs = require('ejs');
+
+const app = express();
+app.set("views", path.join(__dirname, "/views"))
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+    });
+
+app.listen(3000, function () {
+    console.log("Put http://localhost:3000 into the url bar to see your new app"); //String template literal, accuratley shows the port you are serving on Heroku or local. 
+});
